d8c511d84f35ade215dc379ea38589430080a04b
Gjorgji Dimeski <dimeskigj@gmail.com>
2026-01-20 18:55:23 +0100

    feat: update privacy policy url

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,5 +2,5 @@ class Constants {
   static const String baseApiUrl = 'lekovi.dimeski.net';
   static const String lekoviUrl = 'https://lekovi.zdravstvo.gov.mk';
   static const String privacyPolicyUrl =
-      'https://github.com/dimeskigj/DrugRegistry.Flutter/blob/e1166e246c373dcaed327c7af58505b31a1511cc/PRIVACY_POLICY.md';
+      'https://github.com/dimeskigj/drugregistry.flutter/blob/86f5fb435788d44793f172cb422a9068e22afaf6/PRIVACY_POLICY.md';
 }
